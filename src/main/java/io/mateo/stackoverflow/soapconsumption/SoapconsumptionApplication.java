783725d444afb76d2659c70bd26a8caba554229c
ciscoo <cisco21c@gmail.com>
2019-02-05 08:32:14 -0600

    Initial commit

---
+++ src/main/java/io/mateo/stackoverflow/soapconsumption/SoapconsumptionApplication.java
@@ -0,0 +1,14 @@
+package io.mateo.stackoverflow.soapconsumption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapconsumptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapconsumptionApplication.class, args);
+	}
+
+}
+
